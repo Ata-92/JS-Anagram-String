897f6b12fcbd92399dd60cf4c37e0e356a388ce4
Ata-92 <atakal92@gmail.com>
2021-04-22 23:54:37 +0300

    Create a new problem

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-function anagram(text1, text2) {
-  return text1.toLocaleLowerCase().split("").sort().join("") == text2.toLocaleLowerCase().split("").sort().join("");
-}
+// function anagram(text1, text2) {
+//   return text1.toLocaleLowerCase().split("").sort().join("") == text2.toLocaleLowerCase().split("").sort().join("");
+// }
 
-console.log(anagram("Mary", "Army"));
+// console.log(anagram("Mary", "Army"));
+
+
+
+const arr = ['nap', 'teachers', 'cheaters', 'PAN', 'ear', 'era', 'hectares'];
+
+for (let item of arr) {
+
+}
